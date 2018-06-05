cac970876cf5c14364adbf83253ff423dfbe03d9
Zachary Spitzer <zspitzer@nd.edu>
2018-06-05 11:24:52 -0400

    "Homework 1"

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('testing')
+const button1 = document.querySelector('button')
+const button2 = document.querySelector('input[value="Change second header"]')
+
+function differ(){
+    const submit = document.querySelector('#dontChange')
+    submit.textContent = "This is different"
+}
+
+function submission(){
+    const heading = document.querySelector('#Change')
+    const text = document.querySelector('input[name="Input:"]').value
+    heading.textContent = text
+}
+
+function keypress(event){
+    if(event.keyCode == 13) submission
+}
+button1.addEventListener('click', differ)
+button2.addEventListener('click', submission)
+button2.addEventListener('keydown', keypress(event))
