d6eaa822f056b2adeece0716ad1e2d0dfcda564d
Zachary Spitzer <zspitzer@nd.edu>
2018-06-06 12:12:16 -0400

    Separates append function into smaller parts

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const form = document.querySelector('form')
 
 function differ(){
     const submit = document.querySelector('#dontChange')
-    submit.textContent = "Spitzer's Spellbook of Sweet Savvy Spells"
+    submit.textContent = "Spitzer's Spellbook of Special Spiffy Spells"
 }
 
 const submission = function(ev){
@@ -13,32 +13,33 @@ const submission = function(ev){
         window.alert("Error: Spell Name needed")
     }
     else{
+        const heading = headAppend(x)
+        const paragraph = descriptAppend(x)
+        const listSet = listAppend(x, heading, paragraph)
+    x.reset()
+    }
+}
+
+function headAppend(x){
     const heading = document.createElement('h2')
     const title = document.createTextNode(x.Spell.value)
     heading.appendChild(title)
+    return heading
+}
 
+function descriptAppend(x){
     const paragraph = document.createElement('p')
     const description = document.createTextNode(x.Descript.value)
     paragraph.appendChild(description)
+    return paragraph
+}
 
+function listAppend(x, heading, paragraph){
     const listSet = document.querySelector('#Div')
     listSet.appendChild(heading)
     listSet.appendChild(paragraph)
-    }
-    x.reset()
+    return listSet
 }
-
-/*function headAppend(x){
-    const heading = document.createElement('h2')
-    const title = document.createTextNode(x.Spell.value)
-    heading.appendChild(title)
-}*/
-
-/*function descriptAppend(x){
-    const paragraph = document.createElement('p')
-    const description = document.createTextNode(x.Descript.value)
-    paragraph.appendChild(description)
-}*/
 
 function keypress(event){
     if(event.keyCode == 13) submission
